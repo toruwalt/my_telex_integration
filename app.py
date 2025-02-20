e83e3a1649c5bbd7679c5c62ae98684633944b4e
Torubiri Walter <toruwalt1997@gmail.com>
2025-02-20 01:12:06 +0100

    fix: return Telex JSON spec

--- app.py
+++ app.py
@@ -4,7 +4,58 @@ class HelloWorld:
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-        return "Yes oh"
+        telex_json = {
+            "data": {
+                "date": {
+                "created_at": "2024-02-20",
+                "updated_at": "2024-02-20"
+                },
+                "descriptions": {
+                    "app_description": "A real time error log for CherryPy framework that captures and reports errors and exceptions occurring in a CherryPy application.",
+                    "app_logo": "URL to the application logo.",
+                    "app_name": "Errorless Cherry",
+                    "app_url": "URL to the application or service.",
+                    "background_color": "#HEXCODE"
+                },
+                "integration_category": "Communication & Collaboration",
+                "integration_type": "output",
+                "is_active": false, # type: ignore
+                "output": [
+                {
+                    "label": "output_channel_1",
+                    "value": true # type: ignore
+                },
+                {
+                    "label": "output_channel_2",
+                    "value": false # type: ignore
+                }
+                ],
+                "key_features": [
+                "Checks for unhandled error",
+                "Logs error in your app",
+                "Forwards this error to Telex",
+                "Forwards to your developer's forum"
+                ],
+                "permissions": {
+                "monitoring_user": {
+                    "always_online": true, # type: ignore
+                    "display_name": "Performance Monitor"
+                }
+                },
+                "settings": [
+                {
+                    "label": "Do you want to continue",
+                    "type": "checkbox",
+                    "required": true, # type: ignore
+                    "default": "Yes"
+                },
+                ],
+                "tick_url": "URL for subscribing to Telex's clock.",
+                "target_url": "Optional URL for getting data from the Telex channel"
+            }
+        }
+
+        return telex_json
 
 if __name__ == "__main__":
     cherrypy.config.update({
